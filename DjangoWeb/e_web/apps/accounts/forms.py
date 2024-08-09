13fc2abdcbb2c54ff429f67382a2a2ed2e549623
ADyusuf12 <adyusuf68@gmail.com>
2024-08-09 16:11:44 +0100

    cofingured account type 2nd commit

--- DjangoWeb/e_web/apps/accounts/forms.py
+++ DjangoWeb/e_web/apps/accounts/forms.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 
 
 
 class RegisterForm(UserCreationForm):
+    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=True)
+    
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'groups']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'group']
--- DjangoWeb/e_web/apps/accounts/views.py
+++ DjangoWeb/e_web/apps/accounts/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.models import Group, User
 
 
 
@@ -14,8 +15,10 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            group = form.cleaned_data['group']        
+            group.user_set.add(user)
             return redirect('/accounts/login')
-        
-    context = {'form': form}
-    return render(request, 'register.html', context)
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
