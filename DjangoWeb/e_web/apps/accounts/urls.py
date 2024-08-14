3225a6eddc6a8d9f1dc4187dda2b66c04b749a07
ADyusuf12 <adyusuf68@gmail.com>
2024-08-14 23:10:43 +0100

    Profile Settings ( logged in user can view other user's profile) 5th commit

--- DjangoWeb/e_web/apps/accounts/urls.py
+++ DjangoWeb/e_web/apps/accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import ProfileView
 
 
 app_name="accounts"
 urlpatterns = [
     
     
-    path("profile", views.ProfileView.as_view(), name="profile"),
+    path('profile/', ProfileView.as_view(), name="profile"),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='user-profile-detail'),
     path("login", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", views.register, name="register"),
--- DjangoWeb/e_web/apps/accounts/views.py
+++ DjangoWeb/e_web/apps/accounts/views.py
@@ -1,16 +1,19 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegisterForm, ProfileForm
 from django.contrib.auth.decorators import login_required
-from .decorators import allowed_users
 from .models import UserProfile
 
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_profile = get_object_or_404(UserProfile, pk=self.kwargs.get('pk', self.request.user.profile.pk))
+        context['user_profile'] = user_profile
+        return context
 
 @login_required
 def ProfileSettings(request):
