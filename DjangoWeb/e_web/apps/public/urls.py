6d78be736b723d559ea5ab99b92967f7af861e88
ADyusuf12 <adyusuf68@gmail.com>
2024-08-16 23:39:08 +0100

    Configured Password Reset Function

--- DjangoWeb/e_web/apps/public/urls.py
+++ DjangoWeb/e_web/apps/public/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 app_name="public"
@@ -15,6 +16,10 @@ urlpatterns = [
     path('create/', views.properties_list_create, name="create"),
     path('update-listing/<int:pk>/', views.properties_list_update, name="update listing"),
     path('delete-listing/<int:pk>/', views.deleteListing, name="delete-listing"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', success_url=reverse_lazy('public:password_reset_done')), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html', success_url=reverse_lazy('public:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     
     
 ]
